3229c31cb568251a347cf1d703454c1a405e4bcd
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2024-11-12 10:38:14 +0330

    add toman ro card cypto

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 const root = document.querySelector(".root");
 const apiCrypto =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,litecoin,ripple,cardano,polkadot,binancecoin,solana,shiba-inu,monero,stellar,uniswap,chainlink,vechain,algorand,zcash,dogecoin,tron,bitcoin-cash,ethereum-classic,cosmos,tezos,neo,dash,yearn-finance,maker,compound,pancakeswap,busd,shiba,elrond,helium,sushiswap,quickswap,polygon,cosmos,safemoon,celo,uniswap,vault,decenex,fantom,arweave,bancor,renrenbit,litentry,earthquake,powr,klaytn,gala,axie-infinity,bittorrent,curve-finance,serum,tether-usd,sushi,helios,thunder-core,greentrust,terra,stargate,poly,uma,lido,flow,busd,1inch,compound,reef,frax,opensea,dodo,renren,flux,frax,crypto-com-chain,render-token,lido,alpha-finance";
-
 let previousPrices = {};
 
 function cryptoUpdate() {
     fetch(apiCrypto)
-    .then(res => res.json())
+    .then(res => res.json())                    
     .then(data => {
         let temp = data
             .map(items => {
@@ -25,6 +24,7 @@ function cryptoUpdate() {
                         <div class="text__card">
                             <h3 id="name__card">Name: ${items.name}</h3>
                             <h3 id="price__card" style="color:${color};">Price: $${items.current_price}</h3>
+                            <h3 id="price__card1"> Toman ${ (items.current_price * 66000).toLocaleString() }</h3>
                         </div>
                     </div>
                 `;
@@ -34,6 +34,5 @@ function cryptoUpdate() {
         root.innerHTML = temp;  
     });
 }
-
 cryptoUpdate();
-setInterval(cryptoUpdate, 60000);
+setInterval(cryptoUpdate, 1000);
